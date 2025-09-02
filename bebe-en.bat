d1862369b75b7629e57d1794f15bd4671d312ba2
me-suzy <me.suzana@gmail.com>
2025-09-02 23:18:27 +0300

    Add bebe-en.bat

---
+++ bebe-en.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Pornirea scripturilor
+
+start /b c:\Users\necul\AppData\Local\Programs\Python\Python312\python.exe "e:\Carte\BB\17 - Site Leadership\alte\Ionel Balauta\Aryeht\Task 1 - Traduce tot site-ul\Doar Google Web\Andreea\Meditatii\Sedinta 18\Parsing WEBSITE - FINAL.py"
+start /b c:\Users\necul\AppData\Local\Programs\Python\Python312\python.exe "e:\Carte\BB\17 - Site Leadership\alte\Ionel Balauta\Aryeht\Task 1 - Traduce tot site-ul\Doar Google Web\Andreea\Meditatii\Sedinta 4 internet\replace_nbsp_cu_un_singur_spatiu_in_tagurile.py"
+start /b c:\Users\necul\AppData\Local\Programs\Python\Python312\python.exe "e:\Carte\BB\17 - Site Leadership\alte\Ionel Balauta\Aryeht\Task 1 - Traduce tot site-ul\Doar Google Web\Andreea\Meditatii\Sedinta 4 internet\Sterge spatiile goale duble din tagurile (varianta FINALA).py"
+start /b c:\Users\necul\AppData\Local\Programs\Python\Python312\python.exe "e:\Carte\BB\17 - Site Leadership\alte\Ionel Balauta\Aryeht\Task 1 - Traduce tot site-ul\Doar Google Web\Andreea\Meditatii\Sedinta 9 (2022) (EMAIL)\BEBE-PARSING-Python (FARA SUBFOLDER).py" 
+start /b c:\Users\necul\AppData\Local\Programs\Python\Python312\python.exe "e:\Carte\BB\17 - Site Leadership\alte\Ionel Balauta\Aryeht\Task 1 - Traduce tot site-ul\Doar Google Web\Andreea\Meditatii\2023\Schimba tagurile p class text obisnuit2 in H2 si H3.py"
+start /b c:\Users\necul\AppData\Local\Programs\Python\Python312\python.exe "e:\Carte\BB\17 - Site Leadership\alte\Ionel Balauta\Aryeht\Task 1 - Traduce tot site-ul\Doar Google Web\Andreea\Meditatii\2023\inlocuieste-fisiere-gata-design-categorii.py"
+start /b c:\Users\necul\AppData\Local\Programs\Python\Python312\python.exe "e:\Carte\BB\17 - Site Leadership\alte\Ionel Balauta\Aryeht\Task 1 - Traduce tot site-ul\Doar Google Web\Andreea\Meditatii\2023\Inlocuieste fiecare icon-facebook jpg cu imaginea nou creata.py"
+
+
+echo Toate scripturile au fost pornite. Apăsați orice tastă pentru a închide această fereastră.
+pause > nul
