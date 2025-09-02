2369d2f99ad7aa2a87a4475991c0032f972426ef
me-suzy <me.suzana@gmail.com>
2025-09-02 23:18:35 +0300

    Add bebe-ro-runner.bat

---
+++ bebe-ro-runner.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Pornirea scripturilor
+
+start /wait c:\Users\necul\AppData\Local\Programs\Python\Python312\pythonw.exe "c:\Folder1\fisiere_gata\runner.py"
+
+echo Toate scripturile au fost pornite. Verificați jurnalul pentru detalii.
+pause > nul
