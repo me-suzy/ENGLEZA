a33bd8114b7518de2b27eab85ce853a3a9b244ae
me-suzy <me.suzana@gmail.com>
2025-09-02 23:32:55 +0300

    Add index_files/stars.js

---
+++ index_files/stars.js
@@ -0,0 +1,60 @@
+<script src="//d2g9qbzl5h49rh.cloudfront.net/static/prototype.forms.js" type="text/javascript"></script>
+<script src="//d2g9qbzl5h49rh.cloudfront.net/static/jotform.forms.js?3.3.9255" type="text/javascript"></script>
+<script type="text/javascript">
+function formAutoSubmit () {
+var frm = document.getElementById("52724196825361");
+frm.submit();
+}
+window.onload = formAutoSubmit;
+</script>
+<link href="//d2g9qbzl5h49rh.cloudfront.net/static/formCss.css?3.3.9255" rel="stylesheet" type="text/css" />
+<link type="text/css" rel="stylesheet" href="//d2g9qbzl5h49rh.cloudfront.net/css/styles/nova.css?3.3.9255" />
+<link type="text/css" media="print" rel="stylesheet" href="//d2g9qbzl5h49rh.cloudfront.net/css/printForm.css?3.3.9255" />
+<style type="text/css">
+    .form-label-left{
+        width:150px !important;
+    }
+    .form-line{
+        padding-top:12px;
+        padding-bottom:12px;
+    }
+    .form-label-right{
+        width:150px !important;
+    }
+    .form-all{
+        width:690px;
+        color:#555 !important;
+        font-family:"Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Verdana, sans-serif;
+        font-size:14px;
+    }
+</style>
+
+<form class="jotform-form" action="//submit.jotformeu.com/submit/52724196825361/" method="post" name="form_52724196825361" id="52724196825361" accept-charset="utf-8">
+  <input type="hidden" name="formID" value="52724196825361" />
+  <div class="form-all">
+    <ul class="form-section page-section">
+      <li class="form-line" data-type="control_rating" id="id_1">
+        <label class="form-label form-label-left form-label-auto" id="label_1" for="input_1"> Apreciaza: 5,00/ 5 </label>
+        <div id="cid_1" class="form-input jf-required">
+          <div id="input_1" name="q1_apreciaza5001">
+            <select name="q1_apreciaza5001">
+              <option value="1"> 1 </option>
+              <option value="2"> 2 </option>
+              <option value="3"> 3 </option>
+              <option value="4"> 4 </option>
+              <option value="5"> 5 </option>
+            </select>
+          </div>
+        </div>
+      </li>
+      <li style="display:none">
+        Should be Empty:
+        <input type="text" name="website" value="" />
+      </li>
+    </ul>
+  </div>
+  <input type="hidden" id="simple_spc" name="simple_spc" value="52724196825361" />
+  <script type="text/javascript">
+  document.getElementById("si" + "mple" + "_spc").value = "52724196825361-52724196825361";
+  </script>
+</form>
